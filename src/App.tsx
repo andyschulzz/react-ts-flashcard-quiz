ca99c8359700665a7a3e3c77557fe7db0ed5c993
andyschulzz <myandy@gmx.net>
2020-04-17 12:51:50 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FlashcardList from './FlashcardList'
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is 4+4?',
+    answer: '8',
+    options: ['2', '4', '5'],
+  },
+  {
+    id: 2,
+    question: 'What is 8+8?',
+    answer: '8',
+    options: ['2', '4', '5'],
+  },
+]
 
 interface Props {}
 
 const App: React.FC = (props: Props) => {
-  return <div></div>
+  const [flashcards, setFlascards] = useState(SAMPLE_FLASHCARDS)
+  return <FlashcardList flashcards={flashcards} />
 }
 
 export default App
---
+++ src/FlashcardList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface Props {}
+
+const FlashcardList = (props: Props) => {
+  return <div></div>
+}
+
+export default FlashcardList
